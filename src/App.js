505611620d2d030ca44f72d89f3e74d6eacfaa74
rui-watanabe <wataru610806@gmail.com>
2020-04-04 22:06:11 +0900

    implement refetchQueries

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import client from './client'
 import { ADD_STAR, REMOVE_STAR, SEARCH_REPOSITORIES } from './graphql'
 
 const StarButton = props => {
-  const node = props.node
+  const { node, query, first, last, before, after } = props
   const totalCount = node.stargazers.totalCount
   const viewerHasStarred = node.viewerHasStarred
   const starCount = totalCount === 1 ? "1 star" : `${totalCount} stars`
@@ -23,7 +23,17 @@ const StarButton = props => {
   }
 
   return (
-    <Mutation mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}>
+    <Mutation 
+      mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}
+      refetchQueries={mutationResult => {
+        return[
+          {
+            query: SEARCH_REPOSITORIES,
+            variables: {query, first, last, before, after}
+          }
+        ]
+      }}
+    >
       {
         addOrRemoveStar => <StarStatus addOrRemoveStar={addOrRemoveStar}/>
       }
@@ -108,7 +118,7 @@ class App extends Component {
                         <li key = {node.id}>
                           <a href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
                           &nbsp;
-                          <StarButton node={node} />
+                          <StarButton node={node} {...{query, first, last, before, after }}/>
                         </li>
                       )
                     })}
